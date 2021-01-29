62ca3ed37593b834283624ac63eb1667ed11059a
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-01-29 18:43:29 +0300

    Routing library added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useRoutes, Link } from 'raviger';
+
+const routes = {
+  '/': () => <div>/</div>,
+  '/about': () => <div>/about</div>,
+  '/users/:userId': ({ userId } : { [k: string]: string }) => <div>/users/{userId}</div>
+};
+
 const App = () => {
+  let route = useRoutes(routes);
+  
   return (
     <div>
-      yolo
+      <div>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/users/1">Tom</Link>
+        <Link href="/users/2">Jane</Link>
+      </div>
+      {route}
     </div>
   );
-}
+};
 
 export default App;

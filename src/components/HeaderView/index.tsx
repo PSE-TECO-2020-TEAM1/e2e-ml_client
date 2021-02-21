94137399d115b5876663b00aff599eab99b327fc
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-02-21 20:25:51 +0300

    styling done

--- src/components/HeaderView/index.tsx
+++ src/components/HeaderView/index.tsx
@@ -1,31 +1,30 @@
 import { Link } from 'raviger';
 import React from 'react';
 import styles from './index.module.scss';
-const { container } = styles;
+const { container, bread, usernameC, signupC, loginC, seperator, float } = styles;
 
 type Crumb = { name: string, href: string } | string;
 
 export type HeaderViewProps = {
     crumbs?: Crumb[]
-    goBack?: () => void,
     username?: string,
     logout?: () => void,
     login?: string,
     signup?: string
 }
 
-const HeaderView = ({ crumbs, goBack, username, logout, login, signup }: HeaderViewProps) => <header className={container}>
-    <div>{goBack ? <button onClick={goBack}>back</button> : null}</div>
-    <div>
+const HeaderView = ({ crumbs, username, logout, login, signup }: HeaderViewProps) => <header className={container}>
+    <div className={bread}>
         {crumbs ? crumbs.map((crumb, i) => <div key={typeof crumb === 'string' ? crumb : crumb.name}>
-            {i !== 0 ? <div>/</div> : null}
-            <div>{typeof crumb === 'string' ? crumb : <Link href={crumb.href}>{crumb.name}</Link>}</div>
+            {i !== 0 ? <span className={seperator}>/</span> : null}
+            <span>{typeof crumb === 'string' ? crumb : <Link href={crumb.href}>{crumb.name}</Link>}</span>
         </div>) : null}
     </div>
-    <div>{username || null}</div>
-    <div>{logout ? <button onClick={logout}>logout</button> : null}</div>
-    <div>{login ? <Link href={login}>login</Link> : null}</div>
-    <div>{signup ? <Link href={signup}>signup</Link> : null}</div>
+    <div className={float}/>
+    <div className={usernameC}>{username || null}</div>
+    <div>{logout ? <button onClick={logout}>Log out</button> : null}</div>
+    <div className={loginC}>{login ? <Link href={login}>Log in</Link> : null}</div>
+    <div className={signupC}>{signup ? <Link href={signup}>Sign up</Link> : null}</div>
 </header>;
 
 export default HeaderView;

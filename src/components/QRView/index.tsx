84e4c42d808857e5f5fb5b146a61aecaa013ea9a
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-02-21 19:50:12 +0300

    qr style

--- src/components/QRView/index.tsx
+++ src/components/QRView/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
+import Wrapper from 'components/Wrapper';
+import styles from './index.module.scss';
+const { main, qr, descst, link } = styles;
 
 export type QRViewProps = {
     desc: string,
     href: string
 }
 
-const QRView = ({ desc, href } : QRViewProps) => <>
-    <span>{desc}</span>
-    <QRCode value={href} />
-    <a href={href}>{href}</a>
-    <button onClick={() => navigator.clipboard.writeText(href)}>CPY</button>
-</>;
+const QRView = ({ desc, href } : QRViewProps) => <Wrapper className={main}>
+    <span className={descst}>{desc}</span>
+    <QRCode className={qr} value={href} size={400} />
+    <div className={link}>
+        <a href={href}>{href}</a>
+        <button onClick={() => navigator.clipboard.writeText(href)}>copy to clipboard</button>
+    </div>
+</Wrapper>;
 
 export default QRView;

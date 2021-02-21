a8853b35aea535a554f2ee796cc738957f35a389
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-02-21 19:41:46 +0300

    wpage style

--- src/components/WorkspacePageView/index.tsx
+++ src/components/WorkspacePageView/index.tsx
@@ -3,6 +3,8 @@ import SampleList, { SampleListProps } from 'components/SampleList';
 import Wrapper from 'components/Wrapper';
 import { Link } from 'raviger';
 import React from 'react';
+import styles from './index.module.scss';
+const { main, samples, options, labBut, modBut } = styles;
 
 export type WorkspacePageViewProps = {
     sampleProps: SampleListProps,
@@ -11,11 +13,17 @@ export type WorkspacePageViewProps = {
     modelsHref: string,
 }
 
-const WorkspacePageView = ({ sampleProps, modelOptionsProps, labelsHref, modelsHref }: WorkspacePageViewProps) => <Wrapper>
-    <SampleList {...sampleProps} />
-    <Link href={labelsHref}>Labels</Link>
-    <Link href={modelsHref}>Models</Link>
-    <ModelOptions {...modelOptionsProps} />
+const WorkspacePageView = ({ sampleProps, modelOptionsProps, labelsHref, modelsHref }: WorkspacePageViewProps) => <Wrapper className={main}>
+    <div className={samples}>
+        <SampleList {...sampleProps} />
+    </div>
+    <aside>
+        <Link href={labelsHref}><button className={labBut} >Labels</button></Link>
+        <Link href={modelsHref}><button className={modBut} >Models</button></Link>
+        <div className={options}>
+            <ModelOptions {...modelOptionsProps} />
+        </div>
+    </aside>
 </Wrapper>;
 
 export default WorkspacePageView;

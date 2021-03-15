c5933d024849fa386b0f0842dda2182a54bcf011
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-03-15 12:45:23 +0100

    recording configuration and label selection

--- src/components/LabelSelectionPageView/index.tsx
+++ src/components/LabelSelectionPageView/index.tsx
@@ -1,3 +1,4 @@
+import { Heading, Pane, majorScale, Button } from 'evergreen-ui';
 import { Promised, PromisePack } from 'lib/hooks/Promise';
 import { Link } from 'raviger';
 import React from 'react';
@@ -9,14 +10,16 @@ export type LabelSelectionPageViewProps = {
     }[]>
 } 
 
-const LabelSelectionPageView = ({ labelsPH }: LabelSelectionPageViewProps) => <>
-    <header>Choose an action to record:</header>
-    <nav>
-        <Promised promise={labelsPH} pending={'loading...'}>{labels =>
-            labels.map(({ label, href }) =>
-                <Link href={href} key={label} ><button>{label}</button></Link>)
-        }</Promised>
-    </nav>
-</>;
+const LabelSelectionPageView = ({ labelsPH }: LabelSelectionPageViewProps) => 
+    // flex gap doesn't work on mobile browsers yet... see caniuse.com, therefore marginTop
+    <Pane display="flex" flexDirection="column" alignItems="center" padding={majorScale(2)}>
+        <Heading size={500} marginBottom={majorScale(2)}>Choose an action to record:</Heading>
+        <Pane display="grid" gap={majorScale(2)} gridTemplateColumns="repeat(2, minmax(0, 1fr))">
+            <Promised promise={labelsPH} pending={'loading...'}>{labels =>
+                labels.map(({ label, href }) =>
+                    <Button is={Link} href={href} key={label} >{label}</Button>)
+            }</Promised>
+        </Pane>
+    </Pane>;
 
 export default LabelSelectionPageView;
--- src/components/RecordingConfigurationPageView/index.tsx
+++ src/components/RecordingConfigurationPageView/index.tsx
@@ -1,21 +1,28 @@
+import { Button, Heading, majorScale, Pane, TextInput } from 'evergreen-ui';
+import { ETV } from 'lib/utils';
 import { Link } from 'raviger';
 import React from 'react';
 
 export type RecordingConfigurationPageViewProps = {
     href: string,
-    duration: number,
-    countdown: number,
+    duration: number | '',
+    countdown: number | '',
     onDuration: (n: number) => void,
     onCountdown: (n: number) => void,
 } 
 
-const RecordingConfigurationPageView = ({ href, countdown, duration, onDuration, onCountdown }: RecordingConfigurationPageViewProps) => <>
-    <header>Configure Recording Parameters:</header>
-    <ul>
-        <li><em>Countdown: </em><input value={countdown} onChange={e => onCountdown(parseFloat(e.target.value))} type="number" min="0" ></input></li>
-        <li><em>Duration: </em><input value={duration} onChange={e => onDuration(parseFloat(e.target.value))} type="number" min="0" ></input></li>
-    </ul>
-    <Link href={href} ><button>Record</button></Link>
-</>;
+const RecordingConfigurationPageView = ({ href, countdown, duration, onDuration, onCountdown }: RecordingConfigurationPageViewProps) =>
+    <Pane padding={majorScale(2)}>
+        <Pane display="grid" gridTemplateColumns="auto auto" gap={majorScale(2)} alignItems="baseline">
+            <Heading gridColumn="span 2" size={500} >Configure Recording Parameters:</Heading>
+            <Heading size={400}>Countdown: </Heading>
+            <TextInput value={countdown} onChange={(e: ETV<string>) => onCountdown(parseFloat(e.target.value))} type="number" min="0"></TextInput>
+            <Heading size={400}>Duration: </Heading>
+            <TextInput value={duration} onChange={(e: ETV<string>) => onDuration(parseFloat(e.target.value))} type="number" min="0" ></TextInput>
+            <Pane gridColumn={2} display="flex" justifyContent="flex-end">
+                <Button appearance="primary" href={href} is={Link}>Record</Button>
+            </Pane>
+        </Pane>
+    </Pane>;
 
 export default RecordingConfigurationPageView;

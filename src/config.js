bf6b54201f664bce49b816a66d79a33627699f14
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-03-21 04:12:32 +0100

    fix #1 for real now

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || window.location.origin;
+// export const BASE_URL = process.env.REACT_APP_BASE_URL || window.location.origin;
+export const BASE_URL = window.location.origin;
+// TODO: react doesn't have runtime (for docker) envs, use origin as base in the meantime 

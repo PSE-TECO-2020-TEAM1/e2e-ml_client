b6cc41c5cced2b4f8f6535c054cd3e9313f74e9a
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2021-04-11 13:43:04 +0200

    fixes #16

--- src/containers/useWorkspaceCollectDataPage.ts
+++ src/containers/useWorkspaceCollectDataPage.ts
@@ -1,4 +1,5 @@
 import { WorkspaceCollectDataPageViewProps } from 'components/WorkspaceCollectDataPageView';
+import { toaster } from 'evergreen-ui';
 import { useAPI, useAuth, usePromise } from 'lib/hooks';
 import { useHeader } from 'lib/hooks/Header';
 import { createCollectionLink } from 'routes';
@@ -7,6 +8,13 @@ const useWorkspaceCollectDataPage = (workspaceId: string): WorkspaceCollectDataP
     useAuth();
     useHeader({ workspaceId, dangle: 'Collect Data' });
     const api = useAPI();
+
+    usePromise(async () => {
+        const labels = await api.getLabels(workspaceId);
+        if (labels.length === 0) {
+            toaster.warning('You don\'t have any labels yet, please create at least one before collecting samples.');
+        }
+    }, [workspaceId]);
 
     const collectLinkPH = usePromise(async () => createCollectionLink(await api.getDataCollectionID(workspaceId)), [workspaceId]);
 
